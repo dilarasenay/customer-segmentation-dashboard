3e8b5d441edc50f4a15682c20d830072ceb43cf7
BATUHAN INAN <66177097+ahvcxa@users.noreply.github.com>
2026-02-06 18:13:57 +0300

    Delete src/dashboard directory

--- src/dashboard/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-
-main = Blueprint("main", __name__)
-
-@main.route("/")
-def home():
-    return jsonify({
-        "status": "Customer Segmentation Dashboard Running 🚀"
-    })
